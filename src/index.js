d77d382c641b5f74273b241a542f84042503581c
Johann Schopplich <mail@johannschopplich.com>
2021-10-01 11:47:21 +0200

    Uppercase Vue component files (part 1)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import query from "./components/query.vue";
-import queryFieldPreview from "./components/queryFieldPreview.vue";
+import Query from "./components/Query.vue";
+import QueryFieldPreview from "./components/QueryFieldPreview.vue";
 
 window.panel.plugin("rasteiner/k3-query-field", {
   fields: {
-    query,
+    Query,
   },
   components: {
-    "k-query-field-preview": queryFieldPreview,
+    "k-query-field-preview": QueryFieldPreview,
   },
 });
